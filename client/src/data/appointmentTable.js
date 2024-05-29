2137fc7e7df25c544b8b2e4f3fa17a3a46b41212
Haseeb Ali <smhaseebali8@gmail.com>
2024-05-30 00:05:18 +0500

    created data file for appointmentTable.js

--- client/src/data/appointmentTable.js
+++ client/src/data/appointmentTable.js
@@ -1,28 +1,24 @@
 export const tableInfo = [
     {
       s_number: 1,
-      queueNo: 1,
-      opdId: "OP-5",
-      UHID: "U-2/2023",
       patient_name: "Maria Anders",
       Department: "GENERAL MEDICINE",
       Doctor: "Dr. Taha Rehman",
       Type: "General",
       Fee: 500,
       payment_mode: "Cash",
-      Process: "Process"
     },
-    {
-        s_number: 1,
-        queueNo: 1,
-        opdId: "OP-5",
-        UHID: "U-2/2023",
-        patient_name: "Maria Anders",
-        Department: "GENERAL MEDICINE",
-        Doctor: "Dr. Taha Rehman",
-        Type: "General",
-        Fee: 500,
-        payment_mode: "Cash",
-        Process: "Process"
-      }
+    // {
+    //     s_number: 1,
+    //     queueNo: 1,
+    //     opdId: "OP-5",
+    //     UHID: "U-2/2023",
+    //     patient_name: "Maria Anders",
+    //     Department: "GENERAL MEDICINE",
+    //     Doctor: "Dr. Taha Rehman",
+    //     Type: "General",
+    //     Fee: 500,
+    //     payment_mode: "Cash",
+    //     Process: "Process"
+    //   }
   ];
