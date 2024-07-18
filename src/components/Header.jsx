0088fc132d1e49c84946aa05cbf605a8f1dd9cbf
Syed Haseeb Ali <smhaseebali8@gmail.com>
2024-07-18 14:07:20 +0500

    Added Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import logo from "../assets/logo.png"
+const Header = () => {
+    const services = [
+        { name: "OPD", path: "/dashboard/opd", key: 1 },
+        { name: "Queue", path: "/dashboard/queue", key: 2 },
+        { name: "Investigation", path: "/dashboard/investigation", key: 3 },
+        { name: "Day Care", path: "/dashboard/day-care", key: 4 },
+        { name: "Birth Reg", path: "/dashboard/birth-reg", key: 5 },
+        { name: "Notes", path: "/dashboard/notes", key: 6 },
+        { name: "Revisit", path: "/dashboard/revisit", key: 7 },
+        { name: "Certificate", path: "/dashboard/cert", key: 8 },
+        { name: "MIS", path: "/dashboard/opd", key: 9 },
+    ]
+    return (
+        <>
+            <header className='flex justify-between items-center px-3 w-full bg-[#017f82] shadow-xl top-0 sticky z-50 h-14 text-white'>
+                <div className=''>
+                    HMS LOGO
+                </div>
+                <nav className=''>
+                    <ul className='flex align-middle'>
+                        {services.map(service => (<li key={service.key} className='border-white border-x-[.1rem] px-1 '><NavLink to={service.path}>{service.name}</NavLink></li>))}
+                    </ul>
+                </nav>
+                <div>
+                    <button>Logout</button>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
