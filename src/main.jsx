456f1c9f7b19688cb791b4c0ba41f5af9b9ad7e5
Syed Haseeb Ali <smhaseebali8@gmail.com>
2024-07-28 23:27:42 +0500

    configured redux store

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,17 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import { Provider } from "react-redux";
+import { store } from './store/store.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <ToastContainer/>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <ToastContainer />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
